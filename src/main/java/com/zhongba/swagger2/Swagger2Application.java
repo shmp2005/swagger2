27ed2ea581f4f93d39587f3152484913f0377ab3
Tang Jianhua <shmp2005@163.com>
2018-07-07 15:19:58 +0800

    adding classes

--- src/main/java/com/zhongba/swagger2/Swagger2Application.java
+++ src/main/java/com/zhongba/swagger2/Swagger2Application.java
@@ -2,11 +2,13 @@ package com.zhongba.swagger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Swagger2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Swagger2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Swagger2Application.class, args);
+    }
 }
---
+++ src/main/java/com/zhongba/swagger2/bean/JsonResult.java
@@ -0,0 +1,30 @@
+package com.zhongba.swagger2.bean;
+
+public class JsonResult {
+    private String status = null;
+    private Object result = null;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "JsonResult{" +
+                "status='" + status + '\'' +
+                ", result=" + result +
+                '}';
+    }
+}
